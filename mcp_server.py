9303975c761639181d0d108e43c536bc2f25ef0b
thuhang2510 <tunguyet2510@gmail.com>
2025-09-28 20:13:25 +0700

    fix code

--- mcp_server.py
+++ mcp_server.py
@@ -1,39 +1,33 @@
 from mcp.server.fastmcp import FastMCP
 
+# Khởi tạo server
 mcp = FastMCP("info", host="0.0.0.0", port=8000)
 
 @mcp.tool()
-def get_info(name):
+def get_info(name: str):
     """
-    Get info of name includes: age, birtday
+    Get info of name includes: age, birthday
     """
-
-    if "hang" in name:
-        data = {
+    if "hang" in name.lower():
+        return {
             "age": 18,
-            "birtday": "25/10"
+            "birthday": "25/10"
         }
-    else:
-        data = "Không có dữ liệu được lưu trữ"
-
-    return data
+    return "Không có dữ liệu được lưu trữ"
 
 @mcp.tool()
-def get_people_family(name):
+def get_people_family(name: str):
     """
     Get name of all people in family follow name
     """
-
-    if "hang" in name:
-        data = {
+    if "hang" in name.lower():
+        return {
             "father": "Dam VT",
             "mother": "La TT",
             "sister": "Dam HG"
         }
-    else:
-        data = "Không có dữ liệu được lưu trữ"
-
-    return data
+    return "Không có dữ liệu được lưu trữ"
 
 if __name__ == "__main__":
+    # Chỉ cần run trực tiếp, FastMCP Cloud sẽ quản lý asyncio loop
     mcp.run(transport="sse")
